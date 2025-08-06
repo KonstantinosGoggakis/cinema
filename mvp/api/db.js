6dddd348a03c16fa0b0ffb1400f7389393237ce0
Konstantinos <121713917+KonstantinosGoggakis@users.noreply.github.com>
2025-08-07 00:42:41 +0300

    Update db.js

--- mvp/api/db.js
+++ mvp/api/db.js
@@ -1,9 +1,9 @@
 import mysql from 'mysql2';
 
 export const db = mysql.createConnection({
-    host: 'ipv4.kosmidis.me',
+    host: 'YOUR HOST',
     port: 33066,
-    user: 'kgongakis22b',
-    password: '1edc25b8',
-    database: 'kgongakis22b_db1'
+    user: ' YOUR USERNAME ',
+    password: ' YOUR PASSWORD ',
+    database: ' YOUR DB NAME'
 });
